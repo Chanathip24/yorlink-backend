a9372a3af9051a17a9e3395a7a70edc58451ce93
Chanathip24 <160759163+Chanathip24@users.noreply.github.com>
2025-11-18 00:24:19 +0700

    init

---
+++ src/main/java/org/yorlink/yorlinkapi/YorlinkApiApplication.java
@@ -0,0 +1,13 @@
+package org.yorlink.yorlinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YorlinkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YorlinkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yorlink/yorlinkapi/YorlinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yorlink.yorlinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YorlinkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
